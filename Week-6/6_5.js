3fdf4ce63c6ef2b630bed1bf7178bcbfde53382f
Harsh Agrawal <itsharshag@gmail.com>
2022-07-23 16:41:19 +0530

    Add submissions for 4 exercises

---
+++ Week-6/6_5.js
@@ -0,0 +1,32 @@
+// https://www.interviewbit.com/problems/pair-with-given-difference/
+
+{ 
+ //param A : array of integers
+ //param B : integer
+ //return an integer
+	solve : function(A, B){
+        const arr = A
+        const diff = B
+
+        let map = new Map()
+        
+        for(const num of arr){
+            if(map.get(num)){
+                map.set(num, map.get(num)+1)
+            } else{
+                map.set(num, 1)
+            }
+        }
+        for(const num of arr){
+            const otherNum = diff + num
+            const otherNumExists = map.get(otherNum)
+            if(otherNumExists && otherNum != num){
+                return 1
+            }
+            if(otherNumExists && otherNum == num && otherNumExists > 1){
+                return 1
+            }
+        }
+        return 0
+	}
+};
